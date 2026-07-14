0de68cef6992e0574aaebedea12c657941711cad
Akshaya1412 <akshaya14122005@gmail.com>
2026-07-14 14:18:28 +0530

    Create 34-FindFirstAndLastPositionOfElementInSortedArray

---
+++ 34-FindFirstAndLastPositionOfElementInSortedArray/34-FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,32 @@
+// Last updated: 7/14/2026, 2:18:26 PM
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans={-1,-1};
+        ans[0]=binarySearch(nums,target,true);
+        ans[1]=binarySearch(nums,target,false);
+        return ans;
+    }
+    static int binarySearch(int[] nums,int target,boolean findFirst){
+        int s=0,e=nums.length-1;
+        int ans=-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(target>nums[mid]){
+                s=mid+1;
+            }
+            else if (target<nums[mid]){
+                e=mid-1;
+            }
+            else{
+                ans=mid;
+                if(findFirst){
+                    e=mid-1;
+                }
+                else{
+                    s=mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+}
