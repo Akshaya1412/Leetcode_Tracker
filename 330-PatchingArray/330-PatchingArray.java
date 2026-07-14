e1cbaa2bd77db3152db61b72cb77d5e504c23a87
Akshaya1412 <akshaya14122005@gmail.com>
2026-07-14 14:17:51 +0530

    Create 330-PatchingArray

---
+++ 330-PatchingArray/330-PatchingArray.java
@@ -0,0 +1,18 @@
+// Last updated: 7/14/2026, 2:17:50 PM
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss=1;
+        int i=0,patches=0;
+        while(miss<=n){
+            if(i<nums.length&&nums[i]<=miss){
+                miss+=nums[i];
+                i++;
+            }
+            else{
+                miss+=miss;
+                patches++;
+            }
+        }
+        return patches;
+    }
+}
