5fd9f1b1c6758e2df6192ae73b1a91e3aa7f22d9
Akshaya1412 <akshaya14122005@gmail.com>
2026-09-21 21:28:18 +0530

    Create 331-VerifyPreorderSerializationOfABinaryTree

---
+++ 331-VerifyPreorderSerializationOfABinaryTree/331-VerifyPreorderSerializationOfABinaryTree.java
@@ -0,0 +1,20 @@
+// Last updated: 9/21/2026, 9:28:16 PM
+class Solution {
+    public boolean isValidSerialization(String preorder) {
+        String[] nodes=preorder.split(",");
+        int diff=0;
+        for(int i=0;i<nodes.length;i++){
+            if(nodes[i].equals("#")){
+                diff--;
+            }
+            else{
+                diff++;
+            }
+            if(diff==-1&&i!=nodes.length-1){
+                return false;
+            }
+        }
+        return diff==-1;
+
+    }
+}
