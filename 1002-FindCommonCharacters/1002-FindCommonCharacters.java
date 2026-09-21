c594c9562c364bf18a70f4f1fb8c860c6f1827bc
Akshaya1412 <akshaya14122005@gmail.com>
2026-09-21 21:25:01 +0530

    Create 1002-FindCommonCharacters

---
+++ 1002-FindCommonCharacters/1002-FindCommonCharacters.java
@@ -0,0 +1,25 @@
+// Last updated: 9/21/2026, 9:24:59 PM
+class Solution {
+    public List<String> commonChars(String[] words) {
+        List<String> result=new ArrayList<>();
+        for(char c='a';c<='z';c++){
+            int minCount=Integer.MAX_VALUE;
+            for(String word:words){
+                int count=0;
+                for(char ch:word.toCharArray()){
+                    if(ch==c){
+                        count++;
+                    }
+                }
+                minCount=Math.min(minCount,count);
+                if(minCount==0){
+                    break;
+                }
+            }
+            for(int i=0;i<minCount;i++){
+                result.add(String.valueOf(c));
+            }
+        }
+        return result;
+    }
+}
