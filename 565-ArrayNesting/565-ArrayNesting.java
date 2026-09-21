59c7f59ee91213577f04ba0d48a89b50dbb2c7f8
Akshaya1412 <akshaya14122005@gmail.com>
2026-09-21 21:26:55 +0530

    Create 565-ArrayNesting

---
+++ 565-ArrayNesting/565-ArrayNesting.java
@@ -0,0 +1,23 @@
+// Last updated: 9/21/2026, 9:26:53 PM
+class Solution {
+    public int arrayNesting(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=-1){
+                int j=i;
+                int count=0;
+                while(nums[j]!=-1){
+                    int k=nums[j];
+                    nums[j]=-1;
+                    j=k;
+                    count++;
+                }
+                ans=Math.max(ans,count);
+                if(ans==nums.length){
+                    return ans;
+                }
+            }
+        }
+            return ans;
+    }
+}
