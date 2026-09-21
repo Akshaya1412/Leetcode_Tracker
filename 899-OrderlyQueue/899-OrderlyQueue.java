807fea22db620c103e574a44b1495388a506bf5b
Akshaya1412 <akshaya14122005@gmail.com>
2026-09-21 21:25:37 +0530

    Create 899-OrderlyQueue

---
+++ 899-OrderlyQueue/899-OrderlyQueue.java
@@ -0,0 +1,16 @@
+// Last updated: 9/21/2026, 9:25:35 PM
+class Solution {
+        public String orderlyQueue(String S, int K) {
+        if (K > 1) {
+            char S2[] = S.toCharArray();
+            Arrays.sort(S2);
+            return new String(S2);
+        }
+        String res = S;
+        for (int i = 1; i < S.length(); i++) {
+            String tmp = S.substring(i) + S.substring(0, i);
+            if (res.compareTo(tmp) > 0) res = tmp;
+        }
+        return res;
+    }
+}
