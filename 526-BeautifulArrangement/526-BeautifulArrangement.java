94006fc1ceed598ef4970ad734dd0dd9b46edd19
Akshaya1412 <akshaya14122005@gmail.com>
2026-09-21 21:27:04 +0530

    Create 526-BeautifulArrangement

---
+++ 526-BeautifulArrangement/526-BeautifulArrangement.java
@@ -0,0 +1,24 @@
+// Last updated: 9/21/2026, 9:27:01 PM
+class Solution {
+    int res=0;
+    public int countArrangement(int n) {
+        if(n==0)
+            return 0;
+        backtrack(1,n,new int[n+1]);
+        return res;
+        
+    }
+    private void backtrack(int pos,int n,int[] usedValueArr){
+        if(pos>n){
+            res++;
+            return;
+        }
+        for(int i=1;i<=n;i++){
+            if(usedValueArr[i]==0&&(pos%i==0||i%pos==0)){
+                usedValueArr[i]=1;
+                backtrack(pos+1,n,usedValueArr);
+                usedValueArr[i]=0;
+            }
+        }
+    }
+}
