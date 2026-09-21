3a5124cb24c028e181d3a5e17ceed2bee19ed692
Akshaya1412 <akshaya14122005@gmail.com>
2026-09-21 21:26:08 +0530

    Create 771-JewelsAndStones

---
+++ 771-JewelsAndStones/771-JewelsAndStones.java
@@ -0,0 +1,15 @@
+// Last updated: 9/21/2026, 9:26:06 PM
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            for(int j=0;j<jewels.length();j++){
+                if(stones.charAt(i)==jewels.charAt(j)){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
