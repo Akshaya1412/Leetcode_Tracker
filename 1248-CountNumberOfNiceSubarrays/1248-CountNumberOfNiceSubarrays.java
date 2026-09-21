a9cafe9e726e145c0e6eea73c380229bfa5e3bf2
Akshaya1412 <akshaya14122005@gmail.com>
2026-09-21 21:24:40 +0530

    Create 1248-CountNumberOfNiceSubarrays

---
+++ 1248-CountNumberOfNiceSubarrays/1248-CountNumberOfNiceSubarrays.java
@@ -0,0 +1,17 @@
+// Last updated: 9/21/2026, 9:24:38 PM
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n=nums.length;
+        int[] count=new int[n+1];
+        count[0]=1;
+        int ans=0,t=0;
+        for(int v:nums){
+            t+=v&1;
+            if(t-k>=0){
+                ans+=count[t-k];
+            }
+            count[t]++;
+        }
+        return ans;
+    }
+}
