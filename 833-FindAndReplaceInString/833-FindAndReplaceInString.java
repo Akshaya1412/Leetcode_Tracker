4dceb5c3736b6dfeffcf7002879035ceeeaedbc6
Akshaya1412 <akshaya14122005@gmail.com>
2026-09-21 21:25:53 +0530

    Create 833-FindAndReplaceInString

---
+++ 833-FindAndReplaceInString/833-FindAndReplaceInString.java
@@ -0,0 +1,23 @@
+// Last updated: 9/21/2026, 9:25:51 PM
+class Solution {
+    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<indices.length;i++){
+            if(s.startsWith(sources[i],indices[i])){
+                map.put(indices[i],i);
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();){
+            if(!map.containsKey(i)){
+                sb.append(s.charAt(i));
+                i++;
+            }
+            else{
+                sb.append(targets[map.get(i)]);
+                i+=sources[map.get(i)].length();
+            }
+        }
+        return sb.toString();
+    }
+}
